086ed22327c516f6492953778a63c8c957fdf460
Noah Vickerson <noah.vickerson@gmail.com>
2026-02-24 14:26:23 -0700

    Moved over artifact code from artifact pickup script to refactored script. Will remove the legacy script in the future.

--- Assets/Scripts/Maze/Artifact.cs
+++ Assets/Scripts/Maze/Artifact.cs
@@ -2,14 +2,39 @@ using UnityEngine;
 
 public class Artifact : Interactable
 {
-    public override void handleCollision(){
-      return;
+    private Animator animator;
+    private bool isCollected = false;
+
+    public override void handleCollision(Collider2D other)
+    {
+        // Check if player touched the artifact AND it's not already collected
+        if (other.CompareTag("Player") && !isCollected)
+        {
+            isCollected = true;
+
+            // Play the pickup animation
+            if (animator != null)
+            {
+                // Play animation directly by name - NO TRIGGER NEEDED!
+                animator.Play("ArtifactPickup", 0, 0f);
+            }
+
+            // Disable the collider so player can't collect it again
+            GetComponent<Collider2D>().enabled = false;
+
+            // Destroy the artifact after 1 second (after animation finishes)
+            Destroy(gameObject, 1f);
+
+            // TODO: Later we'll add code to show question screen here
+            Debug.Log("Artifact collected! Show question screen next.");
+        }
     }
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Get the Animator component automatically added by Unity
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,4 +42,5 @@ public class Artifact : Interactable
     {
         
     }
+
 }
