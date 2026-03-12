31d453fa72c7598c854a26ad1ebee82faf88ebc1
Kevin <kevin.he1@ucalgary.ca>
2026-03-12 11:24:38 -0600

    Save player progress script
    This script runs once. Connect it to when the player logs out or quits to main menu

---
+++ Assets/Scripts/SaveUserProgressController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class SaveUserProgressController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", PlayerPrefs.GetString("username"));
+        form.AddField("city", PlayerPrefs.GetString("city"));
+        form.AddField("score", PlayerPrefs.GetInt("score"));
+
+        using (UnityWebRequest request = UnityWebRequest.Post("http://localhost/SQLConnect/saveplayerprogress.php", form))
+        {
+            request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Progress saved successfully");
+            }
+            else
+            {
+                Debug.Log("Network Error: " + request.error);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
