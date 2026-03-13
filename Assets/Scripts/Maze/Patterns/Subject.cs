b945063239654c2f19e1701d0c7ca661b3a14b2b
Noah Vickerson <noah.vickerson@gmail.com>
2026-03-12 21:27:58 -0600

    added a floor to the maze. Items and artifact now spawn in the correct places

---
+++ Assets/Scripts/Maze/Patterns/Subject.cs
@@ -0,0 +1,8 @@
+namespace Patterns {
+  public interface Subject
+  {
+    public void addObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+  }
+}
