d840161214d50f147fec5aa431d4a89e3c7a3003
Noah Vickerson <noah.vickerson@gmail.com>
2026-03-21 18:58:18 -0600

    added 2 new items to the maze.

--- Assets/Scripts/Maze/PlayerItemController.cs
+++ Assets/Scripts/Maze/PlayerItemController.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Patterns;
+using UnityEngine.UI;
 
 public class PlayerItemController : MonoBehaviour, Subject
 {
-  List<Observer> observers = new List<Observer>();
+  private List<Observer> observers = new List<Observer>();
+
+  [SerializeField]
+  private Button telescopeButton;
+  [SerializeField]
+  private Button teleporterButton;
 
   // unless we want item persistence, these lists do not matter
   // private List<int> collectedItems;  
@@ -19,11 +25,27 @@ public class PlayerItemController : MonoBehaviour, Subject
 
   public void collectItem(HelperItem.itemName id){
     heldItems.Add(id);
+
+    // handle telescope and teleporter
+    if(id == HelperItem.itemName.Teleport){
+      teleporterButton.interactable = true;
+    }else if(id == HelperItem.itemName.Telescope){
+      telescopeButton.interactable = true;
+    }
+
     notifyObservers();
   }
 
   public void useItem(HelperItem.itemName id){
     heldItems.Remove(id);
+
+    if(id == HelperItem.itemName.Teleport &&
+        !heldItems.Contains(HelperItem.itemName.Teleport)){
+      teleporterButton.interactable = false;
+    }else if(id == HelperItem.itemName.Telescope &&
+        !heldItems.Contains(HelperItem.itemName.Telescope)){
+      telescopeButton.interactable = false;
+    }
   }
 
   public void addObserver(Observer o){
@@ -42,10 +64,16 @@ public class PlayerItemController : MonoBehaviour, Subject
     }
   }
 
+  public void onTelescopeClick(){
+    useItem(HelperItem.itemName.Telescope);
+  }
+
   // Start is called once before the first execution of Update after the MonoBehaviour is created
   void Start()
   {
     heldItems = new List<HelperItem.itemName>();
+    teleporterButton.interactable = false;
+    telescopeButton.interactable = false;
   }
 
   // Update is called once per frame
